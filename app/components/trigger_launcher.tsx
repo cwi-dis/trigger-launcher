eb28744d626ec4050dfcd3e1421dc787bf035862
Thomas Röggla <tom@lichtfragmente.com>
2018-03-29 14:38:24 +0200

    added initial wrapper component for the application

---
+++ app/components/trigger_launcher.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+class TriggerLauncher extends React.Component<{}, {}> {
+  public constructor(props: any) {
+    super(props);
+  }
+
+  public render() {
+    return (
+      <div>Trigger Launcher</div>
+    );
+  }
+}
+
+export default TriggerLauncher;
--- app/main.tsx
+++ app/main.tsx
@@ -3,9 +3,11 @@ import { render } from "react-dom";
 
 import "bulma/css/bulma.css";
 
+import TriggerLauncher from "./components/trigger_launcher";
+
 window.onload = () => {
   render(
-    <div>Hello World!</div>,
+    <TriggerLauncher />,
     document.getElementById("react")
   );
 }
