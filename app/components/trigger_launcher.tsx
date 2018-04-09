bd5f4d179dbafc4b66aef86616dc207f5bde1b75
Thomas Röggla <tom@lichtfragmente.com>
2018-04-09 23:39:49 +0200

    added types for events to TriggerLauncher component

--- app/components/trigger_launcher.tsx
+++ app/components/trigger_launcher.tsx
@@ -1,13 +1,39 @@
 import * as React from "react";
 
-class TriggerLauncher extends React.Component<{}, {}> {
+export type ParamTypes = "duration" | "time" | "string" | "url" | "const" | "set" | "selection";
+
+export interface EventParams {
+  type: ParamTypes;
+  name: string;
+  parameter: string;
+  value?: string;
+  options?: Array<{label: string, value: string}>;
+  required?: boolean;
+}
+
+export interface Event {
+  trigger: boolean;
+  modify: boolean;
+  id: string;
+  parameters: Array<EventParams>;
+  name: string;
+  longdesc?: string;
+  previewUrl?: string;
+  verb?: string;
+}
+
+interface TriggerLauncherProps {
+  documentId: string;
+}
+
+class TriggerLauncher extends React.Component<TriggerLauncherProps, {}> {
   public constructor(props: any) {
     super(props);
   }
 
   public render() {
     return (
-      <div>Trigger Launcher</div>
+      <div>Trigger Launcher for document ID {this.props.documentId}</div>
     );
   }
 }
