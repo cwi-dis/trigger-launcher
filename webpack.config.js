3a0ee597edeeb98511369d63f9611cc982c8ffd4
Thomas Röggla <t.roggla@cwi.nl>
2019-10-15 17:01:51 +0200

    fixed config for css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
         test: /\.css$/,
         use: [
           "style-loader",
-          { loader: "css-loader", options: { "minimize": true } }
+          "css-loader"
         ]
       }
     ]
